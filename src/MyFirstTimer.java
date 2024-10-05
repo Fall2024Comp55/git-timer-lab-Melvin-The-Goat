d2676dc138b9cf4cb725deabdfdb1b4a57716c86
Melvin <m_boateng@u.pacific.edu>
2024-10-05 10:23:59 -0700

    need a timer object

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,20 +1,44 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	private int numTimes ;
+	public Timer timer = new Timer(1000, this);
 	private GLabel myLabel;
+	
 
+	public void actionPerformed(ActionEvent e) {
+		numTimes = numTimes + 1;
+		myLabel.move(5, 0);
+		myLabel.setLabel("times called? " + numTimes);
+		if(numTimes == 10) {
+			timer.stop();
+			return;
+		}
+		//System.out.println("1");
+	}
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
 	
 	public void run() {
+		numTimes = 0;
 		myLabel = new GLabel("# of times called?", 0, 100);
+		
+		System.out.println(numTimes);
 		add(myLabel);
+		
+		
+		timer.start();
 	}
 	
 	public static void main(String[] args) {
