30c4b8fc3371050999f5e93447814bdf51ae3dea
Melvin <m_boateng@u.pacific.edu>
2024-11-04 14:20:26 -0800

    added movement for user character

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,41 +1,66 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.Timer;
 
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
-public class MyFirstTimer extends GraphicsProgram implements ActionListener {
+
+
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener, KeyListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private int numTimes ;
 	public Timer timer = new Timer(1000, this);
 	private GLabel myLabel;
+	private GImage userPlayer = new GImage("src/Images/user2BackProfile.png");
+	private int labelX =400, labelY = 300;
 	
 
 	public void actionPerformed(ActionEvent e) {
-		numTimes = numTimes + 1;
-		myLabel.move(5, 0);
-		myLabel.setLabel("times called? " + numTimes);
-		if(numTimes == 10) {
-			timer.stop();
-			return;
-		}
-		//System.out.println("1");
-	}
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
+		
 	}
 	
-	public void run() {
-		numTimes = 0;
-		myLabel = new GLabel("# of times called?", 0, 100);
+	
+	public void init() {
+		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+		add(userPlayer);
+		userPlayer.setLocation(labelX, labelY);
+		userPlayer.scale(0.2);
+		addKeyListeners();
+		requestFocus();
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
 		
-		System.out.println(numTimes);
-		add(myLabel);
+		switch(keyCode) {
+		case KeyEvent.VK_W://up
+			labelY -= 10;
+			System.out.println("Moving Up");
+			break;
+		case KeyEvent.VK_S://down
+			labelY += 10;
+			System.out.println("Moving Down");
+			break;
+		case KeyEvent.VK_A://left
+			labelX -= 10;
+			System.out.println("Moving Left");
+			break;
+		case KeyEvent.VK_D://right
+			labelX += 10;
+			System.out.println("Moving Right");
+			break;
+		}
+		userPlayer.setLocation(labelX, labelY);
+	}
+	public void run() {
 		
 		
 		timer.start();
@@ -45,3 +70,4 @@ public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 		new MyFirstTimer().start();
 	}
 }
+
